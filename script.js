7455706b2678fc81949db367106680cb64bbe787
Toba <toba_ojo@hotmail.com>
2020-10-04 12:42:52 +0100

    added functionality for  sort, add and double

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const main = document.getElementById('main');
 const addUser = document.getElementById('add-user');
-const doubleMoney = document.getElementById('double-money');
+const doubleMoneyBtn = document.getElementById('double-money');
 const showMillion = document.getElementById('show-millionaires');
-const sort = document.getElementById('sort');
+const sortBtn = document.getElementById('sort');
 const calWealth = document.getElementById('calculate-wealth');
 
 
@@ -17,6 +17,61 @@ getRandomUser()
 async function getRandomUser(){
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
+    const user = data.results[0];
 
-    console.log(data);
+    const newUser ={
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+    addData(newUser);
+
 }
+
+function doubleMoney(){
+    data = data.map((user) => {
+        return {...user, money: user.money * 2}
+    });
+    
+    updateDom();
+}
+
+function sortByRichest(){
+    data.sort((a,b) => b.money - a.money);
+
+    updateDom();
+}
+
+//add data to function array
+function addData(obj){
+    data.push(obj);
+
+    updateDom()
+}
+
+//double eveyone money
+
+
+//update dom
+
+function updateDom(provData = data){
+    //clear main div
+    main.innerHTML = `<h2><strong>Person </strong>wealth</h2>`
+    provData.forEach( item => {
+        const element = document.createElement('div');
+            element.classList.add('person');
+            element.innerHTML = `<strong>${item.name}</strong>${formatMoney(item.money)}`
+            main.appendChild(element)
+    })
+}
+
+//format numer as money 
+
+function formatMoney(number){
+    return '£'+number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
+//event Listener
+addUser.addEventListener('click', getRandomUser);
+doubleMoneyBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest)
